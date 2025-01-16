581449c3bc6f59566e573868327f914febdb4bc1
mohdanas86 <anasalam7739@gmail.com>
2025-01-16 22:05:59 +0530

    contact btn update

--- app/_components/Contact.jsx
+++ app/_components/Contact.jsx
@@ -129,7 +129,7 @@ function Contact() {
                 type="submit"
                 className="w-full bg-blue-600 text-white px-4 py-2 font-bold rounded-md hover:opacity-80"
               >
-                Update Profile
+                Contact
               </Button>
             </form>
           </Form>
