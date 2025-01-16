0b6387c4e4635759754572b89e9980a4cbedfca2
mohdanas86 <anasalam7739@gmail.com>
2025-01-16 21:41:06 +0530

    key

--- util/mongodb.js
+++ util/mongodb.js
@@ -2,8 +2,6 @@
 import mongoose from "mongoose";
 
 // MongoDB connection URI (use environment variable for security)
-const MONGODB_URI =
-  "mongodb+srv://anas:anas@food.t6wubmw.mongodb.net/portfolio?retryWrites=true&w=majority&appName=food";
 
 // Connection function using Mongoose
 export async function connectToDatabase() {
